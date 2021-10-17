8f8bef7e52449e94888752dfda7dfae98c086a18
nftug <whitepony1214@gmail.com>
2021-10-17 17:49:50 +0900

    npm run serveでも動くように修正

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   outputDir: '../static',
   indexPath: '../templates/index.html',
-  publicPath: process.env.NODE_ENV === 'production' ? '/static/' : '/',
-  productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template:
-        process.env.NODE_ENV === 'production'
-          ? 'public/index.html'
-          : 'public/index_dev.html',
-      title: 'DRF Sample',
-    },
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  productionSourceMap: process.env.NODE_ENV === 'production' ? true : true,
+
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template:
+          process.env.NODE_ENV === 'production'
+            ? 'public/index.html'
+            : 'public/index_dev.html',
+        favicon: './public/favicon.ico',
+        inject: process.env.NODE_ENV === 'production' ? false : true,
+        minify: false,
+      }),
+    ],
   },
 }
